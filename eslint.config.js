5fa4372560ae33010531f57d0ff596b9ebe7a5e8
chukuwuma4u2c <chukwuma4u2c@gmail.com>
2026-06-07 18:21:02 +0100

    fix: properly configure

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,37 @@
 import js from "@eslint/js";
-import eslintConfigNext from "eslint-config-next";
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import tseslint from "typescript-eslint";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
 
-export default [
-  js.configs.recommended,
-  eslintConfigNext,
-  eslintPluginPrettier,
+export default tseslint.config(
   {
+    ignores: [".next/**", "dist/**"],
+  },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintConfigPrettier,
+    ],
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      "react-hooks": eslintPluginReactHooks,
+      prettier: eslintPluginPrettier,
+    },
     rules: {
+      ...eslintPluginReactHooks.configs.recommended.rules,
+      "prettier/prettier": "warn",
       "@typescript-eslint/no-unused-vars": "off",
     },
-  },
-];
+  }
+);
+
